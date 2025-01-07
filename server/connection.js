1e29865f04a957a51892bb976b840d4ee905e0d6
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2025-01-07 16:11:21 +0530

    Update connection.js

--- server/connection.js
+++ server/connection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
-    const conn=await mongoose.connect("mongodb+srv://innovest:innovest@cluster0.vtkziqm.mongodb.net/",{dbName:"InnoVest"}).then(()=>{
+    const conn=await mongoose.connect(process.env.databaseurl,{dbName:"InnoVest"}).then(()=>{
         console.log("MongoDB Connected");
     }).catch((err)=>{
         console.log(`error occured:(${err}`);
